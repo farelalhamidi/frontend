bad3a37bca913d4a81db81da0bca21fceb7fafcd
farel <m.farelalhamidi8.1@gmail.com>
2024-07-09 21:10:41 -0700

    membuat components kotak 1 dan kotak 2 dan di tampilkan di App

---
+++ frontend/src/components/Kotak1.jsx
@@ -0,0 +1,10 @@
+import Componentw from 'react'
+function Kotak1() {
+  return (
+    <>
+        <h2>Kotak 1</h2>
+    </>
+  )
+}
+
+export default Kotak1
---
+++ frontend/src/components/Kotak2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Kotak2() {
+  return (
+    <>
+    <h2>Kotak2</h2>
+    </>
+  )
+}
+
+export default Kotak2
